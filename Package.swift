3c869eba1599e97c4034766632194acd5f5b0955
Cameron Augustine <mtcameron5@gmail.com>
2021-04-21 15:30:58 -0600

    Updated package to import apple login

--- Package.swift
+++ Package.swift
@@ -11,7 +11,9 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0")
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor-community/Imperial.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0")
     ],
     targets: [
         .target(
